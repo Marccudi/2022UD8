0143393f78fea31898df882e4717f8c95ca38e28
Marccudi <marccudi@hotmail.es>
2022-07-13 09:33:36 +0200

    Ejercicio 2 acabado

---
+++ TA8Ej2/src/Password.java
@@ -0,0 +1,33 @@
+
+public class Password {
+
+	int longitud=8;
+	String pass;
+	
+	public Password() {
+	}
+	
+	public Password(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+
+	
+	
+}
