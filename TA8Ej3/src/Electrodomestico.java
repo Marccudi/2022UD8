afc8a0cf8f5be4b07af59867c286a245b391891f
Marccudi <marccudi@hotmail.es>
2022-07-13 10:06:00 +0200

    atributos por defecto usan las constantes sin constructor

--- TA8Ej3/src/Electrodomestico.java
+++ TA8Ej3/src/Electrodomestico.java
@@ -1,36 +1,32 @@
 
 public class Electrodomestico {
-
-	double precioBase;
-	String color;
-	char consumoEnergetico;
-	double peso;
-	
-	
-	final double precioBaseDef=100;
-	final String colorDef="blanco";
-	final char consumoDef='F';
-	final double pesoDef=5;
 	
 	final String arrayColores[]= {"blanco", "negro", "rojo", "azul", "gris"};
 	final char arrayConsumo[]= {'A', 'B', 'C', 'D', 'E', 'F'};
 	
+	final double precioBaseDef=100;
+	final String colorDef=arrayColores[0];
+	final char consumoDef=arrayConsumo[5];
+	final double pesoDef=5;
+	
+
+	double precioBase=precioBaseDef;
+	String color=colorDef;
+	char consumoEnergetico = consumoDef;
+	double peso = pesoDef;
+	
+	
+	
+	
 	public Electrodomestico() {
-		this.precioBase = precioBaseDef;
-		this.color = colorDef;
-		this.consumoEnergetico = consumoDef;
-		this.peso = pesoDef;
 	}
 
 	public Electrodomestico(double precioBase, double peso) {
-		this.color = colorDef;
-		this.consumoEnergetico = consumoDef;
 		this.precioBase = precioBase;
 		this.peso = peso;
 	}
 
 	public Electrodomestico(double precioBase, String color, char consumoEnergetico, double peso) {
-		super();
 		this.precioBase = precioBase;
 		this.color = color;
 		this.consumoEnergetico = consumoEnergetico;
